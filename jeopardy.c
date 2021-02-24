dcd29f1dcfaa04c6c9c43abb4e7a41fb4e37283e
fazemahn <fazemahn@gmail.com>
2021-02-23 20:53:27 -0500

    finalized game logic

--- jeopardy.c
+++ jeopardy.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 
     //Variable to check if all questions are answered. 
     bool cont = true;
-    
+    int turn = 0; 
     // Perform an infinite loop getting command input from users until game ends
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL && cont)
     {
@@ -54,11 +54,27 @@ int main(int argc, char *argv[])
         for (int i = 0; i < NUM_QUESTIONS; i++)
         if (!questions[i].answered)
         cont = true;
-
         //Game Logic is executed. 
-        
-
+        //Display the remaining questions. 
+        display_categories();
+        //Select the category and value. 
+        int selectedVal;
+        char* selectedCat [100];
+        scanf("%d", &selectedVal);
+        gets(selectedCat);
+        //Display the question with the selected category and value. 
+        display_question(selectedCat, selectedVal);
+        //Get the player's answer. 
+        char* playerAns [100];
+        gets(playerAns);
+        //Check if the answer is correct. 
+        bool correct = valid_answer(selectedCat, selectedVal, playerAns);
+        //We use turn to keep track of turn and divide by the num players. The remainder is the current player. 
+        if(correct){
+            update_score(players, NUM_PLAYERS, players[turn % NUM_PLAYERS].name, selectedVal);
+        }
         // Display the final results and exit
+        turn ++;
     }
     return EXIT_SUCCESS;
 }
